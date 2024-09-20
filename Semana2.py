c91bb24b820c0121b32394c7ce5b02c1d182a6d0
EELLX12 <Jazicse92@gmail.com>
2024-09-19 22:23:11 -0500

    definioendo valores enteros

---
+++ Semana2.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+#titulo a la aplicacion
+st.title("Introduccion ava♫")
+
+#descripcionon
+st.write("phyton es un")
+
+#seccion
+st.header("ejemplo 1: enteros")
+st.write(" en phyton un entero")
+
+#definir una vaZ
+int_variable = 42
+st.code(f" int_variable = {int_variable}# tipo {type(int_variable)}")
