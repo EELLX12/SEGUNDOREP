e808a45da8d38bb79485de17b3a47dd9b01f7365
EELLX12 <Jazicse92@gmail.com>
2024-10-18 10:20:07 -0500

    Terminado

--- ejerciciosidl03.py
+++ ejerciciosidl03.py
@@ -3,22 +3,38 @@ import streamlit as st
 #Ejercicio 4:  calcular la suma  de n numeros elevados a la n
 st.title("SESION 12 Estructura repetitiva")
 st.subheader("Ejercicio 4 : suma  de n numeros elevados a la n")
+#Función para generar serie
 def generar_serie(n):
     serie = []
     for i in range(n):
         a=i+1
         b= a**a
         serie.append(b)
-        st.write(serie[i])
-        st.write(a)
     return serie
-    
-
+#Función para validar y calcular la suma
+def calcular_suma_serie(n):
+    if n <= 0:
+        return "El valor de n debe de ser mayor que 0"
+        serie = generar_serie(n)
+        suma = sum(serie)
+    return suma, serie
+st.markdown("""
+<style>
+div.stButton > button{
+background-color: blue;
+color: white;
+font-size: 20px;
+padding; 10px;
+border-radius: 112px;
+border: none;
+}
+</style>
+""", unsafe_allow_html=True)
+#Ingresa el valor de n
 n = st.number_input("ingresa el valor de n:", min_value=1, step=1)
 serie = generar_serie(n)
-st.write(f"La serie generada para n={n} es: {serie}")
-
-lst = [10,20,30,40] 
-x = 1 
-lst.append(x) 
-st.write(lst) 
+#Botón para calcular la suma
+if st.button("Calcular suma"):
+    suma,serie = calcular_suma_serie(n)
+    st.write(f"La serie generada para n={n} es: {serie}")
+    st.write(f"La suma de los primeros {n} elementos es: {suma}")
