411931fb8a75789638754a97b684521e4cd759f4
EELLX12 <Jazicse92@gmail.com>
2024-10-18 10:54:52 -0500

    co2

--- ejerciciosidl03.py
+++ ejerciciosidl03.py
@@ -38,7 +38,7 @@ n = st.number_input("cuantos numeros va ingresar n:", min_value=1, step=1)
 numeros=[]
 i=0
 while i<n:
-    b = input(f"Ingrese el  numero {i+1} :")
+    b = input("Ingrese el  numero  :")
     numeros.append(b)
     i+=i
     
