d9596c225a51ea3b340886c45471047a785e4a29
EELLX12 <Jazicse92@gmail.com>
2024-10-18 10:07:54 -0500

    conta

--- ejerciciosidl03.py
+++ ejerciciosidl03.py
@@ -5,14 +5,14 @@ st.title("SESION 12 Estructura repetitiva")
 st.subheader("Ejercicio 4 : suma  de n numeros elevados a la n")
 def generar_serie(n):
     serie = []
-    for i in n:
+    for i in range(n):
         a=i+1
         serie.append(a^a)
         st.write(serie[i])
         st.write(a)
-        i+=i
+        
     return serie
-
+    i+=i
 
 n = st.number_input("ingresa el valor de n:", min_value=1, step=1)
 serie = generar_serie(n)
