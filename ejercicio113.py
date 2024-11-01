2b83e3bcdaf6780a478c2f20e9113834533b9f6f
EELLX12 <Jazicse92@gmail.com>
2024-11-01 08:19:51 -0500

    aver

--- ejercicio113.py
+++ ejercicio113.py
@@ -3,6 +3,9 @@ import numpy as np
 
 st.title("Ejercicio 1 de 13 : ingresar numeros hasta que se introdusca cero, imprimir suma y media ")
 
+if 'numeros' not in st.session_state:
+    st.session_state.numeros = []
+
 def main():
     
     
@@ -13,19 +16,19 @@ def main():
         numero = st.number_input("Ingresa un número (0 para salir)", step=1.0)
         
         if numero == 0:
-           st.write("salir.")
-           break
-        else:
-            numeros.append(numero)
-    else:
-        st.write("No se ingresaron números.")
+        break
 
-    
-    suma = sum(numeros)
-    media =np.mean(numeros)
+    st.session_state.numeros.append(numero)
+
+# Calcular la suma total
+if st.session_state.numeros:
+    suma = sum(st.session_state.numeros)
+    media =np.mean(st.session_state.numeros)
     st.write(f"La suma de los números ingresados es: {suma}")
     st.write(f"La media de los números ingresados es: {media}")
-    
+else:
+    st.write("No se ingresaron números.")
+       
 
 if __name__ == "__main__":
     main()
