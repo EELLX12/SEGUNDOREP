bd55e8249f74cb47cc08127f74a8a175ea6ff085
EELLX12 <Jazicse92@gmail.com>
2024-11-01 10:03:28 -0500

    menu

---
+++ ejercicomenu.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import numpy as np
+#Ejercicio 3:  CREA UN MENU
+st.title("SESION 13 Estructura repetitiva")
+st.subheader("Ejercicio 3 : GENERA UN MENU ")
+# Crear un menú en la barra lateral
+menu1 = st.sidebar.selectbox("Menús", ["Archivo", "Editar", "Ver", "Salir"])
+
+# Mostrar el contenido según la opción seleccionada
+if menu1 == "Archivo":
+    st.write("Has seleccionado la opción 'Archivo'. Aquí puedes manejar archivos.")
+        
+elif menu1 == "Editar":
+    st.write("Has seleccionado la opción 'Editar'.")
+    
+elif menu1 == "Ver":
+    st.write("Has seleccionado la opción 'Ver'.")
+    
+elif menu1 == "Salir":
+    st.write("Has seleccionado 'Salir'. Gracias por utilizar la aplicación.")
