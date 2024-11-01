fe474cda82f92a62ebd43a4d09fd6a3cbe839acc
EELLX12 <Jazicse92@gmail.com>
2024-11-01 08:07:20 -0500

    if por while

--- ejercicio113.py
+++ ejercicio113.py
@@ -9,21 +9,22 @@ def main():
     # Lista para almacenar los números ingresados
     numeros = []  
 
-    while True:
+    if True:
         numero = st.number_input("Ingresa un número (0 para salir)", step=1.0)
         
         if numero == 0:
             break
         
         numeros.append(numero)
-
-    if numeros:
-        suma = sum(numeros)
-        media =np.mean(numeros)
-        st.write(f"La suma de los números ingresados es: {suma}")
-        st.write(f"La media de los números ingresados es: {media}")
     else:
         st.write("No se ingresaron números.")
+
+    
+    suma = sum(numeros)
+    media =np.mean(numeros)
+    st.write(f"La suma de los números ingresados es: {suma}")
+    st.write(f"La media de los números ingresados es: {media}")
+    
 
 if __name__ == "__main__":
     main()
