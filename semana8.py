30b58dd351124e4a34d83d9c7c594ae7aca1f13e
EELLX12 <Jazicse92@gmail.com>
2024-11-01 07:29:12 -0500

    area y oerimetro

--- semana8.py
+++ semana8.py
@@ -35,3 +35,34 @@ def main():
     st.write(f"Resultado: {resultado}")
 if __name__ == "__main__":
     main()
+
+
+
+#------------------------------------2do ejemplo-----------------------------------
+
+import streamlit as st
+import math
+
+#las funciones se declaran como def
+
+def calcular_area(radio):
+    return math.pi * (radio**2)
+
+def calcular_perimetro(radio):
+    return 2*math.pi * radio
+
+def main():
+    st.title("Calculo de area y perimetro de circunferencia")
+    #Entrada de datos
+    radio = st.number_input("Ingrese el radio de circunferencia:", min_value = 0.0, step=0.1)
+    
+   
+    if radio > 0:
+        area = calcular_area(radio)
+        perimetro = calcular_perimetro(radio)
+    
+    #monstrar el resultado
+    st.write(f"Area : {area:2f}")
+    st.write(f"Perimetro : {perimetro:2f}")
+if __name__ == "__main__":
+    main()
